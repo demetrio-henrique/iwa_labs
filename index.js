4782cdeadfcd4f9beb1e7a23893528cd0c521114
Henrique Demetrio <demetrio.henrique@outlook.com>
2022-05-23 18:26:07 +0000

    Connecting MongoDB

--- index.js
+++ index.js
@@ -11,8 +11,19 @@ app.use(require('./router'));
 app.use(bodyParser.json());
 app.use(logger("tiny"));
 
-app.listen(port, function(err){
-console.log("Listening on port: " +port)
+mongoose.connect('mongodb://localhost/test'); //connecting via localhost with a database called test.
 
+mongoose.connection.on('error', (err) => {
+console.log('Mongodb Error: ', err);
+process.exit();
+
+});
+mongoose.connection.on('connected', () =>{
+  console.log('Mongodb is successfully connected');
+
+});
+
+app.listen(port, function(err){
+    console.log("Listening on port: " +port)
 });
     
