d994358715bb86edecf4ab6e2b37ca7213e418b4
Henrique Demetrio <demetrio.henrique@outlook.com>
2022-05-23 19:34:55 +0000

    user-controller creation and user.js

--- router.js
+++ router.js
@@ -1,9 +1,12 @@
 const express = require("express"),
       router = express.Router(),
       itemController = require("./item-controller"); //importing the item controller class.
+      userController = require("./user-controller");
 
 
       
       router.get("/:foo/:bar", itemController.helloWorld); //accessing the router handling functions to show the function from itemcontroller class.
+      router.post('/users', userController.createUser);
+      router.get ('/users', userController.getUsers);
         
       module.exports = router;
