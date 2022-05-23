3eeedd0e92de911ab324999124a99e7edb56a743
Henrique Demetrio <demetrio.henrique@outlook.com>
2022-05-23 11:18:34 +0000

    WEB SERVER CREATED AND CONSUMED AN API

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require("http"),
+      axios = require("axios");
+
+      //creating a server
+
+      http.createServer((req, res)=>{
+        res.write(users.join(", "));
+        res.end()
+
+      }).listen(8000);
+      
+      let users = [];
+      
+      (async function getNames(){
+        try{
+            const {data} = await axios.get("https://jsonplaceholder.typicode.com/users");
+            users = data.map(user=>user.name)
+
+        }catch(error){
+            console.log(error)
+        }
+        })()
