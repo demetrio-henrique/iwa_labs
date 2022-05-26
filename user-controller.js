a3640cf7c4aa69f7e233f0fa9c5fa3094d1240c2
Henrique Demetrio <demetrio.henrique@outlook.com>
2022-05-26 21:05:52 +0000

    INSERTED FUNCTIONS TO THE USER-CONTROLLER FILE

--- user-controller.js
+++ user-controller.js
@@ -1,23 +1,20 @@
-const User = require("./models/user");
+const User = require("./models/user"); //importing the library  .model . user inserted in the router folder
+//the routes js file has methods/funcionts that are invoking the following.
 
 exports.createUser = function (req,res){
     let newuser = new User(req.body);
     newuser.save(function(err,user){
         if(err){
-            res.status(400).json(err);
+            res.status(400).json(err); //if it fail, then we show the error
         }
-        res.json(user);
+        res.json(user); // if everything is fina, it create a new user.
     });
-
     };
-
-exports.getUsers = function(req,res){
-    User.find({}, function(err,users){
-        if(err){
-            res.status(400).json(err);
-
-        }
-    
-        res.json(users);
+    exports.getUsers = function(req,res){
+        User.find({}, function(err,users){ //im not sure if User is defined properly
+            if (err){
+                res.status(400).json(err);
+            }
+            res.json(users);
     });
-};
+    };
